b1d6c31d86a5ac83e8addbb87ab96d0f158ab654
Koxtx <corentin.lavarde@gmail.com>
2024-09-24 16:24:11 +0200

    design5

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -36,7 +36,7 @@ export default function Profile() {
   };
 
   return (
-    <main>
+    <main className="mhFull">
       <h2>{user.username}</h2>
 
       {!showForm && (
--- src/pages/error404/Error404.jsx
+++ src/pages/error404/Error404.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import error from "../../image/error.jpg";
+import styles from "./Error.module.scss";
 
 export default function Error404() {
   return (
-    <main className="mhFull">
-      <h2>404</h2>
+    <main
+      className="mhFull"
+      style={{
+        backgroundImage: `url(${error})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <h1> ERROR 404</h1>
+      <p>
+        Le site est allé jusqu’à l echec ! Il a besoin de son temps de repos !
+        nous revennons trés vite{" "}
+      </p>
     </main>
   );
 }
