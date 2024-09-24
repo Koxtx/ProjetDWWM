12ea1939843f4b3273e345e7a8f4173cc6597e14
Koxtx <corentin.lavarde@gmail.com>
2024-09-24 14:58:12 +0200

    design4

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,33 @@
+import { NavLink } from "react-router-dom";
 import styles from "./Footer.module.scss";
+import logo from "../../image/logo.png";
 
-export default function Footer(){
-
-    return(
-        <footer>
-
-        </footer>
-    )
+export default function Footer() {
+  return (
+    <footer>
+      <NavLink className={`mr-15`} to="/">
+        <img src={logo} className={`${styles.logo} `} alt="GoIt" />
+      </NavLink>
+      <div className="d-flex flex-column center m-20">
+        <ul className="d-flex flex-column center">
+          <l1>
+            <NavLink className={`mr-15`} to="//mentionlegal">
+              Mentions légales
+            </NavLink>
+          </l1>
+          <l1>
+            <NavLink className={`mr-15`} to="/politiquedeconfidentialité">
+              Politique de confidentialité
+            </NavLink>
+          </l1>
+          <l1>Email : corentin.lavarde@gmail.com</l1>
+          <l1>Numéro de téléphone : 0659296782</l1>
+          <l1>Adresse : 74 rue Harry,auchel , France</l1>
+        </ul>
+      </div>
+      <div>
+        <p>© 2024 GoIt. Tous droits réservés.</p>
+      </div>
+    </footer>
+  );
 }
